f74c4a6586e0fe406c3a27c1910bbafea6c60dad
Hamdan-Khan <hamdankhan212@gmail.com>
2024-11-05 23:38:05 +0500

    dashboard page added

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import GithubSignOutButton from "@/components/GithubSignOutButton";
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <GithubSignOutButton />
+    </div>
+  );
+}
