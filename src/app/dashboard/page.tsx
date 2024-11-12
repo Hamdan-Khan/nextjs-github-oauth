dc68ea63a531a3e9d1dc145f3f2a99ab802d3307
Hamdan-Khan <hamdankhan212@gmail.com>
2024-11-12 15:25:12 +0500

    dashboard profile comp added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,21 @@
-import GithubSignOutButton from "@/components/GithubSignOutButton";
+import Profile from "@/components/dashboard/Profile";
 import React from "react";
+import { SessionManager } from "../api/session";
+import GithubSignInButton from "@/components/GithubLoginButton";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await SessionManager.getSession();
+  if (!session)
+    return (
+      <div>
+        <p>You need to be logged in to access this resource</p>
+        <GithubSignInButton />
+      </div>
+    );
+
   return (
     <div>
-      <GithubSignOutButton />
+      <Profile session={session} />
     </div>
   );
 }
