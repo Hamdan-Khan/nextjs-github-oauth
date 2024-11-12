5015087cd2f2f8c595a07c5e03d56448c8f2a2c7
Hamdan-Khan <hamdankhan212@gmail.com>
2024-11-12 15:24:23 +0500

    home page modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,13 @@
-import Image from "next/image";
-import { SessionManager } from "./api/session";
-import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import GetStarted from "@/components/GetStarted";
 
 export default async function Home() {
-  const session = await SessionManager.getSession();
-
   return (
-    <div>
-      {session ? (
-        <div className="text-white">
-          <h1>Welcome, {session.name}!</h1>
-          <Image
-            src={session.avatar_url}
-            alt={session.name}
-            width={100}
-            height={100}
-            className="rounded-[50%] overflow-hidden"
-          />
-          <p>GitHub Username: {session.login}</p>
-          <p>GitHub Email: {session.email}</p>
-        </div>
-      ) : (
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <Hero />
-          <Features />
-          <GetStarted />
-        </div>
-      )}
+    <div className="flex flex-col min-h-screen">
+      <Hero />
+      <Features />
+      <GetStarted />
     </div>
   );
 }
